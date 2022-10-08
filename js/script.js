98b2f3b643e1378527e0dbd4c3dec438ecf695c8
Alan <ansarialan31@gmail.com>
2022-10-08 16:24:33 +0530

    added bottom border

--- js/script.js
+++ js/script.js
@@ -19,23 +19,26 @@ let grid1 = [
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,1,1,0,0,0,0],
-    [0,0,0,0,1,1,0,0,0,0]
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0]
 ];
 
 
 
 
-for(let i=0;i<20;i++){
+for(let i=0;i<21;i++){
     for(let j=0;j<10;j++){
         const newDiv = document.createElement("div");
         newDiv.setAttribute("id","block-"+i+"-"+j);
         newDiv.style.top = i*4 + 'vh';
         newDiv.style.left = j*4 + 'vh';
-        if(grid1[i][j]==1)
+        if(grid1[i][j]==1 && i!=20)
         newDiv.setAttribute("class","block");
-        else
+        else if(i!=20)
         newDiv.setAttribute("class","no-block");
+        else
+        newDiv.setAttribute("class","border");
         const addNode = document.getElementById("console");
         addNode.appendChild(newDiv);
     }
